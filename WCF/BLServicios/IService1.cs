1b4ba27101ddffe9ac9dc8dabc07cddae7dc9ca1
gaston-git <alfurga@gmail.com>
2018-01-12 12:14:45 -0600

    WCF

---
+++ WCF/BLServicios/IService1.cs
@@ -0,0 +1,48 @@
+﻿using System.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BLServicios
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        //ESTADOS
+        [OperationContract]
+        string getEstados(string IdE);
+
+        [OperationContract]
+        string CrearEstado(string nombre);
+
+        [OperationContract]
+        string EditarEstado(string nombre, string id);
+
+        [OperationContract]
+        string EliminarEstado(string id);
+
+        //REGISTROS
+        [OperationContract]
+        string getRegistros(string idL);
+
+        //SISTEMAS
+        [OperationContract]
+        string getSistemas(string idS);
+
+        //ACCIONES
+        [OperationContract]
+        string getAcciones(string idA);
+
+        //UNIDADES
+        [OperationContract]
+        string getUnidades(string idU);
+    }
+
+
+    
+}
---
+++ WCF/ClassLibrary1/ManAcciones.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+
+namespace AccesoDatos
+{
+    public class ManAcciones
+    {
+        public static string getAcciones(string idA)
+        {//ejecuta una consulta a la BD
+            string resultado = string.Empty;
+            List<dynamic> lista = new List<dynamic>();
+            try
+            {
+                SqlCommand _comando = Metodos.CrearComando();
+                if (idA.Equals("0"))
+                    _comando.CommandText = @"select a.id, a.nombre, a.idEstado, e.nombre as Estado
+                                            from Acciones a, Estados e
+                                            where a.idEstado = e.id
+                                            order by a.nombre";
+                else
+                    _comando.CommandText = @"select a.id, a.nombre, a.idEstado, e.nombre as Estado
+                                            from Acciones a, Estados e
+                                            where a.idEstado = e.id
+                                            and a.id =  "+idA+" order by a.nombre";
+
+                DataTable Dt = Metodos.EjecutarComandoSelect(_comando);
+
+                foreach (DataRow row in Dt.Rows)
+                {
+                    lista.Add(new
+                    {
+                        id = row["id"].ToString().Trim(),
+                        nombre = row["nombre"].ToString().Trim(),
+                        idEstado = row["idEstado"].ToString().Trim(),
+                        Estado = row["Estado"].ToString().Trim()
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                lista.Add("Error: " + e.Message);
+            }
+
+            resultado = JsonConvert.SerializeObject(lista, Newtonsoft.Json.Formatting.Indented);
+            return resultado;
+        }
+
+    }
+}
